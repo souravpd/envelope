f98e5025f686cb4db1a1e714a2116b61633efe35
Sourav Prasad <souravpd@outlook.com>
2021-12-25 20:57:11 +0530

    Minor Code Refactoring

--- test_parser.py
+++ test_parser.py
@@ -5,7 +5,7 @@ from parser import parse
 
 class TestParser(unittest.TestCase):
 
-    def test_tokenize(self):
+    def test_parse(self):
         test_input = """
         layout:post;
         title:Two Sum Problem in LeetCode;
@@ -20,7 +20,7 @@ class TestParser(unittest.TestCase):
         """
         test_answer = [['layout', 'post'], ['title', 'Two Sum Problem in LeetCode'], ['slug', 'two-sum'], ['heading', 'Two Sum'], ['para', 'This is an [bold(implementation)] of the popular [link(leetcode.com/problems/two-sum Two-Sum)] Problem in LeetCode'], ['subheading', 'Explanation of the Code'], [
             'image', '[path(backgorund.png This is the alternate text)]'], ['list', 'This is a list item,This is another list item'], ['task-list', '[False(This is the task)],[True(This is another task)]'], ['para', 'This is some [highlight(random explanation)] for the code that has been written for more information']]
-            
+
         self.assertListEqual(parse(test_input), test_answer,
                              "There is an error in the tokenizer")
 
